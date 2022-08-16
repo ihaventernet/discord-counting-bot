70e5dccdc585f028e5fe5051f4304714f0a4f118
ihaventernet <62654745+ihaventernet@users.noreply.github.com>
2022-08-16 10:07:56 +0700

    Delete screen.py

--- screen.py
+++
@@ -1,9 +0,0 @@
-# Importing Image and ImageGrab module from PIL package 
-from PIL import Image, ImageGrab
-import cv2   
-import numpy as nm
-# using the grab method
-im2 = ImageGrab.grab(bbox = (370, 913, 1000, 940))
-gry = cv2.cvtColor(nm.array(im2), cv2.COLOR_BGR2GRAY)    
-filename =  "test.png"
-cv2.imwrite(filename, gry)
